f3224ea5adbbe72dbf77580427407fabbe8c58da
Gregfanyan <grigorfanyan@gmail.com>
2020-05-26 22:43:07 +0200

    added endpoint for dinamic  number of category

--- routes/category.js
+++ routes/category.js
@@ -12,6 +12,24 @@ router.get("/category/category", (req, res) => {
     });
 });
 
+router.get("/category/category", (req, res) => {
+  pool
+    .query('SELECT * FROM category')
+    .then((data) => res.json(data))
+    .catch((e) => {
+      res.sendStatus(404), console.log(e);
+    });
+});
+
+
+router.get("/category/:id", (req, res) => {
+  const { id } = req.params;
+  pool
+    .query('SELECT * FROM category WHERE id=$1;', [id])
+    .then(data => res.json(data)) 
+    .catch(e => res.sendStatus(404)); 
+ });
+
 
 
 
